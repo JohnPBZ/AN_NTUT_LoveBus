4d7a43355e0a08a8f891cb0d49d20fb854b4a6ab
PBZ <t107360119@ntut.org.tw>
2020-12-19 03:46:15 +0800

    Creating a Tab Layout with Icons & Fragments in Android Studio Tutorial (Kotlin)

--- app/src/main/java/com/example/an_ntut_lovebus/MainActivity.kt
+++ app/src/main/java/com/example/an_ntut_lovebus/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.an_ntut_lovebus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.an_ntut_lovebus.fragments.FavoritesFragment
+import com.example.an_ntut_lovebus.fragments.HomeFragment
+import com.example.an_ntut_lovebus.fragments.SettingsFragment
+import com.example.an_ntut_lovebus.fragments.adapters.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpTabs()
+    }
+    private fun setUpTabs(){
+        val adapter=ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment(),"Home")
+        adapter.addFragment(FavoritesFragment(),"Favorites")
+        adapter.addFragment(SettingsFragment(),"Settings")
+        viewPager.adapter=adapter
+        tabs.setupWithViewPager(viewPager)
     }
 }
