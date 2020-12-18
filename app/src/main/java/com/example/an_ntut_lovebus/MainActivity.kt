f171a28e41f5f3252189c27681216fd66506c95a
PBZ <t107360119@ntut.org.tw>
2020-12-19 02:10:48 +0800

    first commit

---
+++ app/src/main/java/com/example/an_ntut_lovebus/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.an_ntut_lovebus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
